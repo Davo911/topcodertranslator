b7773fb3b6c56324e3fd636fe0a402ae23070dcc
Thomas-David Griedel <thomas-david.griedel2@htw-dresden.de>
2021-08-09 02:55:59 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/topcodertranslator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.topcodertranslator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+Future<http.Response> request_translation(String language, String text) async {
+  print("fetching..");
+  final response = await http.get(Uri.parse(
+      'https://translation.googleapis.com/language/translate/v2?target=${language}&key=AIzaSyCvBxhLZc8tjqKoVuejptU1gXRdRrj2zbc&q=${text}'));
+  //https://jsonplaceholder.typicode.com/albums/1
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return response;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController textController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  List<String> _languages = ['en', 'de', 'hu', 'vi'];
+  String _dropdownValue = "hu";
+  String translation = "";
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Scaffold(
+      key: scaffoldKey,
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment(0, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                child: TextFormField(
+                  controller: textController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Text...',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              DropdownButton(
+                items: _languages.map((lang) {
+                  return DropdownMenuItem(
+                    child: new Text(lang),
+                    value: lang,
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _dropdownValue = newValue!;
+                  });
+                },
+                value: _dropdownValue,
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints.tightFor(width: 300),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    print('Button pressed ...');
+                    var res = await request_translation(
+                        _dropdownValue, textController.text);
+                    Map<String, dynamic> JSONresponse = jsonDecode(res.body);
+                    print(JSONresponse);
+                    setState(() {
+                      translation = JSONresponse['data']['translations'][0]
+                          ['translatedText'];
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.translate,
+                        size: 15,
+                      ),
+                      Text('Translate'),
+                      Icon(
+                        Icons.translate,
+                        size: 15,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Text(
+                  translation,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
